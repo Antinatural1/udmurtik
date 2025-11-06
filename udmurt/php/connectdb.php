95b7ea4aada529b15015acde49a593cf67899a83
Dmitrii1 <165987599+Antinatural1@users.noreply.github.com>
2025-11-06 21:36:38 +0400

    Update connectdb.php

--- udmurt/php/connectdb.php
+++ udmurt/php/connectdb.php
@@ -1,5 +1,6 @@
 <?php
-$conn = new mysqli("localhost", "root", "root", "udmurtik");
+$conn = new mysqli(process.env.DB_HOST, process.env.DB_USERNAME, process.env.DB_PASSWORD, process.env.DB_DBNAME);
 if ($conn->connect_error) {
     die("Ошибка: " . $conn->connect_error);
+
 }
